a2ca7cad794b19f94c51feda1e7a20d1c93fe477
Shawn Hafizh <shawnhafizh@gmail.com>
2022-05-27 15:46:47 +0700

    Changes

--- app/src/main/java/com/capstone/surehealth/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/surehealth/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.capstone.surehealth
+package com.capstone.surehealth.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.surehealth.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
