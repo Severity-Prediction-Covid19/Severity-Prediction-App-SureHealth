5fb90cfd0e58a7d07ed88c3d9b78ef970703695f
Shawn Hafizh <shawnhafizh@gmail.com>
2022-05-28 13:04:03 +0700

    Lot of Changes

---
+++ app/src/main/java/com/capstone/surehealth/ui/quiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.surehealth.ui.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.surehealth.R
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
